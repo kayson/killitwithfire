9540b9eb612b6c3e9efc52748b1f9d1515a44005
Johan Noren <johbe559@student.liu.se>
2013-02-07 16:59:19 +0100

    La till korrekt dx,dy,dz i upwind-diff

--- source/UpwindDiff.cpp
+++ source/UpwindDiff.cpp
@@ -1,5 +1,5 @@
 #include "UpwindDiff.h"
-
+#include "fire.h"
 /*
 	Beräknar andra ordningens approximation av första-upwind differentiation.
 	Index är eg. halva index för staggered MAC-grid.
@@ -9,23 +9,23 @@ UpwindDiff::UpwindDiff(){}
 data UpwindDiff::calcDx(Grid &g, const int i, const int j, const int k)
 {
 	if((*_w)(i, j, k).x > 0)
-		return (g(i, j, k) - g(i-1, j, k))/GRID_SIZE;
+		return (g(i, j, k) - g(i-1, j, k))/FirePresets::dx;
 	else
-		return (g(i+1, j, k) - g(i, j, k))/GRID_SIZE;
+		return (g(i+1, j, k) - g(i, j, k))/FirePresets::dx;
 }
 data UpwindDiff::calcDy(Grid &g, const int i, const int j, const int k)
 {
 	if((*_w)(i, j, k).y > 0)
-		return (g(i, j, k) - g(i, j-1, k))/GRID_SIZE;
+		return (g(i, j, k) - g(i, j-1, k))/FirePresets::dx;
 	else
-		return (g(i, j+1, k) - g(i, j, k))/GRID_SIZE;
+		return (g(i, j+1, k) - g(i, j, k))/FirePresets::dx;
 }
 data UpwindDiff::calcDz(Grid &g, const int i, const int j, const int k)
 {
 	if((*_w)(i, j, k).z > 0)
-		return (g(i, j, k) - g(i, j, k-1))/GRID_SIZE;
+		return (g(i, j, k) - g(i, j, k-1))/FirePresets::dx;
 	else
-		return (g(i, j, k+1) - g(i, j, k))/GRID_SIZE;
+		return (g(i, j, k+1) - g(i, j, k))/FirePresets::dx;
 }
 
 data UpwindDiff::calcD2x(Grid &g, const int i, const int j, const int k)
