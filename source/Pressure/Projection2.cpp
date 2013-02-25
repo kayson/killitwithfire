78271665222b6e7b73012dec585a555b20929b0a
Johannes <johannes@deligiannis.se>
2013-02-25 14:40:34 +0100

    Ersatt projection2 mot Project2D

--- source/Pressure/Projection2.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Projection2.cpp
-//  FuidFire
-//
-//  Created by Johannes Deligiannis on 2/22/13.
-//  Copyright (c) 2013 Johannes Deligiannis. All rights reserved.
-//
-
-#include "Projection2.h"
